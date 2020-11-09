0ef530216d9abc784002176fcb33b5fc7d553d93
clearloop <udtrokia@gmail.com>
2020-11-09 18:37:56 +0800

    feat(dev-exectutor): init dev executor

--- bin/node-dev/cli/src/service.rs
+++ bin/node-dev/cli/src/service.rs
@@ -5,7 +5,9 @@ use std::sync::Arc;
 use sc_client_api::RemoteBackend;
 use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
-use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
+use sc_service::{
+    construct_service_factory, error::Error as ServiceError, Configuration, TaskManager,
+};
 use sp_inherents::InherentDataProviders;
 
 use jupiter_dev_runtime::{self, RuntimeApi};
@@ -19,6 +21,12 @@ native_executor_instance!(
     jupiter_dev_runtime::api::dispatch,
     jupiter_dev_runtime::native_version,
 );
+
+construct_service_factory! {
+    struct Factory {
+        RuntimeDispatch = jupiter_dev_executor::Exectutor;
+    }
+}
 
 type FullClient = sc_service::TFullClient<Block, RuntimeApi, Executor>;
 type FullBackend = sc_service::TFullBackend<Block>;
