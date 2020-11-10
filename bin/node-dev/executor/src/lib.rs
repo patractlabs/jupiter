0d1e6b672241df1c9865e465757c7a21d7fb40e5
clearloop <udtrokia@gmail.com>
2020-11-10 14:30:26 +0800

    docs(primitives/io): add inline tests for the io crate

--- bin/node-dev/executor/src/lib.rs
+++ bin/node-dev/executor/src/lib.rs
@@ -1,3 +1,5 @@
+//! A `CodeExecutor` specialization which uses natively compiled runtime when the wasm to be
+//! executed is equivalent to the natively compiled code.
 use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
 
--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -6,16 +6,27 @@ use sp_runtime_interface::runtime_interface;
 #[runtime_interface]
 pub trait ZkSnarks {
     /// altbn_128 add
+    ///
+    /// ```
+    /// assert_eq!(jupiter_io::zk_snarks::altbn_128_add(1, 1), 2);
+    /// ```
     fn altbn_128_add(lhs: u32, rhs: u32) -> u32 {
         lhs + rhs
     }
 
     /// altbn_128 mul
+    ///
+    /// ```
+    /// assert_eq!(jupiter_io::zk_snarks::altbn_128_mul(2, 2), 4);
+    /// ```
     fn altbn_128_mul(lhs: u32, rhs: u32) -> u32 {
-        lhs + rhs
+        lhs * rhs
     }
 
     /// altbn_128 pairing
+    /// ```
+    /// assert_eq!(jupiter_io::zk_snarks::altbn_128_pairing(2, 2), (0, 2, 0, 2));
+    /// ```
     fn altbn_128_pairing(lhs: u32, rhs: u32) -> (u32, u32, u32, u32) {
         (0, lhs, 0, rhs)
     }
