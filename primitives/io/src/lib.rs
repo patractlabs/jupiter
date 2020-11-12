6aa6d65ae52ad36b44865a18e08df4caac441e3c
clearloop <udtrokia@gmail.com>
2020-11-12 19:22:11 +0800

    fix(primitives/io): the outdated tests

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -6,8 +6,7 @@ use sp_runtime_interface::runtime_interface;
 /// ZK-SNARKs runtime interface
 #[runtime_interface]
 pub trait ZkSnarks {
-    // /// altbn_128 add
-    // ///
+    /// altbn_128 add
     // /// ```
     // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_add(1, 1), 2);
     // /// ```
@@ -19,8 +18,7 @@ pub trait ZkSnarks {
         res
     }
 
-    // /// altbn_128 mul
-    // ///
+    /// altbn_128 mul
     // /// ```
     // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_mul(2, 2), 4);
     // /// ```
@@ -32,7 +30,7 @@ pub trait ZkSnarks {
         res
     }
 
-    // /// altbn_128 pairing
+    /// altbn_128 pairing
     // /// ```
     // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_pairing(2, 2), (0, 2, 0, 2));
     // /// ```
