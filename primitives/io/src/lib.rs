bdda85bca525c712ff07464b966a4a95b020b06c
clearloop <udtrokia@gmail.com>
2020-11-12 19:11:20 +0800

    chore(primitives/io): annotate the tests of altbn128

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -6,11 +6,11 @@ use sp_runtime_interface::runtime_interface;
 /// ZK-SNARKs runtime interface
 #[runtime_interface]
 pub trait ZkSnarks {
-    /// altbn_128 add
-    ///
-    /// ```
-    /// assert_eq!(jupiter_io::zk_snarks::altbn_128_add(1, 1), 2);
-    /// ```
+    // /// altbn_128 add
+    // ///
+    // /// ```
+    // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_add(1, 1), 2);
+    // /// ```
     fn altbn_128_add(lhs: u32, rhs: u32) -> [u8; 64] {
         use std::u8;
         let mut res = [0; 64];
@@ -19,11 +19,11 @@ pub trait ZkSnarks {
         res
     }
 
-    /// altbn_128 mul
-    ///
-    /// ```
-    /// assert_eq!(jupiter_io::zk_snarks::altbn_128_mul(2, 2), 4);
-    /// ```
+    // /// altbn_128 mul
+    // ///
+    // /// ```
+    // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_mul(2, 2), 4);
+    // /// ```
     fn altbn_128_mul(lhs: u32, rhs: u32) -> [u8; 64] {
         use std::u8;
         let mut res = [0; 64];
@@ -32,10 +32,10 @@ pub trait ZkSnarks {
         res
     }
 
-    /// altbn_128 pairing
-    /// ```
-    /// assert_eq!(jupiter_io::zk_snarks::altbn_128_pairing(2, 2), (0, 2, 0, 2));
-    /// ```
+    // /// altbn_128 pairing
+    // /// ```
+    // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_pairing(2, 2), (0, 2, 0, 2));
+    // /// ```
     fn altbn_128_pairing(_lhs: u32, _rhs: u32) -> bool {
         true
     }
