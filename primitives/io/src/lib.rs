df08c9de0c842305cdabe778cf3ff5449201dfec
clearloop <udtrokia@gmail.com>
2020-11-23 16:55:48 +0800

    refactor(jupiter-io): add bls12_381 interfaces

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -1,32 +1,47 @@
 //! Jupiter primitives - IO Module
 #![deny(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
+use megaclite::{altbn_128, bls12_381};
 use sp_runtime_interface::runtime_interface;
 
 /// ZK-SNARKs runtime interface
 #[runtime_interface]
 pub trait ZkSnarks {
     /// altbn_128 add
-    // /// ```
-    // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_add(1, 1), 2);
-    // /// ```
-    fn altbn_128_add(_g1: &[u8], _g2: &[u8]) -> [u8; 64] {
-        [0; 64]
+    fn altbn_128_add(input: &[u8], output: &mut [u8]) -> bool {
+        altbn_128::add(input, output).is_ok()
     }
 
     /// altbn_128 mul
-    // /// ```
-    // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_mul(2, 2), 4);
-    // /// ```
-    fn altbn_128_mul(_input: &[u8], _scalar: u64) -> [u8; 64] {
-        [1; 64]
+    fn altbn_128_mul(input: &[u8], output: &mut [u8]) -> bool {
+        altbn_128::mul(input, output).is_ok()
     }
 
     /// altbn_128 pairing
-    // /// ```
-    // /// assert_eq!(jupiter_io::zk_snarks::altbn_128_pairing(2, 2), (0, 2, 0, 2));
-    // /// ```
-    fn altbn_128_pairing() -> bool {
-        true
+    fn altbn_128_pairing(input: &[u8]) -> bool {
+        if let Ok(res) = altbn_128::pairing(input) {
+            res
+        } else {
+            false
+        }
+    }
+
+    /// bls12_381 add
+    fn bls12_381_add(input: &[u8], output: &mut [u8]) -> bool {
+        bls12_381::add(input, output).is_ok()
+    }
+
+    /// bls12_381 mul
+    fn bls12_381_mul(input: &[u8], output: &mut [u8]) -> bool {
+        bls12_381::mul(input, output).is_ok()
+    }
+
+    /// bls12_381 pairing
+    fn bls12_381_pairing(input: &[u8]) -> bool {
+        if let Ok(res) = bls12_381::pairing(input) {
+            res
+        } else {
+            false
+        }
     }
 }
