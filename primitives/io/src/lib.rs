78ae6aee7efadbb9c1acb413ef154a9e24d498d4
clearloop <udtrokia@gmail.com>
2020-11-24 11:33:43 +0800

    refactor(primitives/io): adapt the new api of megaclite

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -8,13 +8,21 @@ use sp_runtime_interface::runtime_interface;
 #[runtime_interface]
 pub trait ZkSnarks {
     /// altbn_128 add
-    fn altbn_128_add(input: &[u8], output: &mut [u8]) -> bool {
-        altbn_128::add(input, output).is_ok()
+    fn altbn_128_add(input: &[u8]) -> [u8; 64] {
+        if let Ok(res) = altbn_128::add(input) {
+            res
+        } else {
+            [0; 64]
+        }
     }
 
     /// altbn_128 mul
-    fn altbn_128_mul(input: &[u8], output: &mut [u8]) -> bool {
-        altbn_128::mul(input, output).is_ok()
+    fn altbn_128_mul(input: &[u8]) -> [u8; 64] {
+        if let Ok(res) = altbn_128::mul(input) {
+            res
+        } else {
+            [0; 64]
+        }
     }
 
     /// altbn_128 pairing
@@ -27,13 +35,21 @@ pub trait ZkSnarks {
     }
 
     /// bls12_381 add
-    fn bls12_381_add(input: &[u8], output: &mut [u8]) -> bool {
-        bls12_381::add(input, output).is_ok()
+    fn bls12_381_add(input: &[u8]) -> [u8; 96] {
+        if let Ok(res) = bls12_381::add(input) {
+            res
+        } else {
+            [0; 96]
+        }
     }
 
     /// bls12_381 mul
-    fn bls12_381_mul(input: &[u8], output: &mut [u8]) -> bool {
-        bls12_381::mul(input, output).is_ok()
+    fn bls12_381_mul(input: &[u8]) -> [u8; 96] {
+        if let Ok(res) = bls12_381::mul(input) {
+            res
+        } else {
+            [0; 96]
+        }
     }
 
     /// bls12_381 pairing
