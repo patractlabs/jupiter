15cc25cd7a3f2b7c04e9ac6b092fb048a10784cb
clearloop <udtrokia@gmail.com>
2020-11-24 12:12:07 +0800

    feat(primitives/io): imports megaclite inside the runtime_interface block

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -1,7 +1,6 @@
 //! Jupiter primitives - IO Module
 #![deny(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
-use megaclite::{altbn_128, bls12_381};
 use sp_runtime_interface::runtime_interface;
 
 /// ZK-SNARKs runtime interface
@@ -9,7 +8,7 @@ use sp_runtime_interface::runtime_interface;
 pub trait ZkSnarks {
     /// altbn_128 add
     fn altbn_128_add(input: &[u8]) -> [u8; 64] {
-        if let Ok(res) = altbn_128::add(input) {
+        if let Ok(res) = megaclite::altbn_128::add(input) {
             res
         } else {
             [0; 64]
@@ -18,7 +17,7 @@ pub trait ZkSnarks {
 
     /// altbn_128 mul
     fn altbn_128_mul(input: &[u8]) -> [u8; 64] {
-        if let Ok(res) = altbn_128::mul(input) {
+        if let Ok(res) = megaclite::altbn_128::mul(input) {
             res
         } else {
             [0; 64]
@@ -27,7 +26,7 @@ pub trait ZkSnarks {
 
     /// altbn_128 pairing
     fn altbn_128_pairing(input: &[u8]) -> bool {
-        if let Ok(res) = altbn_128::pairing(input) {
+        if let Ok(res) = megaclite::altbn_128::pairing(input) {
             res
         } else {
             false
@@ -36,7 +35,7 @@ pub trait ZkSnarks {
 
     /// bls12_381 add
     fn bls12_381_add(input: &[u8]) -> [u8; 96] {
-        if let Ok(res) = bls12_381::add(input) {
+        if let Ok(res) = megaclite::bls12_381::add(input) {
             res
         } else {
             [0; 96]
@@ -45,7 +44,7 @@ pub trait ZkSnarks {
 
     /// bls12_381 mul
     fn bls12_381_mul(input: &[u8]) -> [u8; 96] {
-        if let Ok(res) = bls12_381::mul(input) {
+        if let Ok(res) = megaclite::bls12_381::mul(input) {
             res
         } else {
             [0; 96]
@@ -54,7 +53,7 @@ pub trait ZkSnarks {
 
     /// bls12_381 pairing
     fn bls12_381_pairing(input: &[u8]) -> bool {
-        if let Ok(res) = bls12_381::pairing(input) {
+        if let Ok(res) = megaclite::bls12_381::pairing(input) {
             res
         } else {
             false
