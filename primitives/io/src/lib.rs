e072d70454610286b7875efc2bec7aa1931ae412
clearloop <udtrokia@gmail.com>
2020-11-27 10:34:41 +0800

    perf(frame/contracts): use option as result of zk curves

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -7,56 +7,32 @@ use sp_runtime_interface::runtime_interface;
 #[runtime_interface]
 pub trait ZkSnarks {
     /// altbn_128 add
-    fn altbn_128_add(input: &[u8]) -> [u8; 64] {
-        if let Ok(res) = megaclite::altbn_128::add(input) {
-            res
-        } else {
-            [0; 64]
-        }
+    fn altbn_128_add(input: &[u8]) -> Option<[u8; 64]> {
+        megaclite::altbn_128::add(input).ok()
     }
 
     /// altbn_128 mul
-    fn altbn_128_mul(input: &[u8]) -> [u8; 64] {
-        if let Ok(res) = megaclite::altbn_128::mul(input) {
-            res
-        } else {
-            [0; 64]
-        }
+    fn altbn_128_mul(input: &[u8]) -> Option<[u8; 64]> {
+        megaclite::altbn_128::mul(input).ok()
     }
 
     /// altbn_128 pairing
-    fn altbn_128_pairing(input: &[u8]) -> bool {
-        if let Ok(res) = megaclite::altbn_128::pairing(input) {
-            res
-        } else {
-            false
-        }
+    fn altbn_128_pairing(input: &[u8]) -> Option<bool> {
+        megaclite::altbn_128::pairing(input).ok()
     }
 
     /// bls12_381 add
-    fn bls12_381_add(input: &[u8]) -> [u8; 96] {
-        if let Ok(res) = megaclite::bls12_381::add(input) {
-            res
-        } else {
-            [0; 96]
-        }
+    fn bls12_381_add(input: &[u8]) -> Option<[u8; 96]> {
+        megaclite::bls12_381::add(input).ok()
     }
 
     /// bls12_381 mul
-    fn bls12_381_mul(input: &[u8]) -> [u8; 96] {
-        if let Ok(res) = megaclite::bls12_381::mul(input) {
-            res
-        } else {
-            [0; 96]
-        }
+    fn bls12_381_mul(input: &[u8]) -> Option<[u8; 96]> {
+        megaclite::bls12_381::mul(input).ok()
     }
 
     /// bls12_381 pairing
-    fn bls12_381_pairing(input: &[u8]) -> bool {
-        if let Ok(res) = megaclite::bls12_381::pairing(input) {
-            res
-        } else {
-            false
-        }
+    fn bls12_381_pairing(input: &[u8]) -> Option<bool> {
+        megaclite::bls12_381::pairing(input).ok()
     }
 }
