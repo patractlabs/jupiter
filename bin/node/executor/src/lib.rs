68f25a9437b38750ee7975936a94ed06544705ba
Aten <jincxmain@gmail.com>
2020-12-03 18:35:26 +0800

    tmp support local contracts crate for jupiter

--- bin/node/executor/src/lib.rs
+++ bin/node/executor/src/lib.rs
@@ -7,5 +7,5 @@ native_executor_instance!(
     pub Executor,
     jupiter_runtime::api::dispatch,
     jupiter_runtime::native_version,
-    frame_benchmarking::benchmarking::HostFunctions,
+    (frame_benchmarking::benchmarking::HostFunctions, jupiter_io::zk_snarks::HostFunctions),
 );
