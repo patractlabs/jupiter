4cff4c9a8afee39a79abcd5d552de9a0438c3b81
clearloop <udtrokia@gmail.com>
2020-12-07 16:22:39 +0800

    feat(primitives/io): update the usage of megaclite

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -6,33 +6,66 @@ use sp_runtime_interface::runtime_interface;
 /// ZK-SNARKs runtime interface
 #[runtime_interface]
 pub trait ZkSnarks {
-    /// altbn_128 add
-    fn altbn_128_add(input: &[u8]) -> Option<[u8; 64]> {
-        megaclite::altbn_128::add(input).ok()
+    /// Vector Addition
+    fn add(fn_id: i32, input: &[u8], output: &mut [u8]) -> Option<()> {
+        let res = match fn_id {
+            0x2a => {
+                <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::add(input).ok()
+            }
+            0x2b => {
+                <megaclite::curves::Bls12_381 as megaclite::CurveBasicOperations>::add(input).ok()
+            }
+            0x2c => <megaclite::curves::Bn254 as megaclite::CurveBasicOperations>::add(input).ok(),
+            0x2d => {
+                <megaclite::curves::BW6_761 as megaclite::CurveBasicOperations>::add(input).ok()
+            }
+            _ => None,
+        }?;
+
+        output.copy_from_slice(&res);
+        Some(())
     }
 
-    /// altbn_128 mul
-    fn altbn_128_mul(input: &[u8]) -> Option<[u8; 64]> {
-        megaclite::altbn_128::mul(input).ok()
+    /// Scalar Multiplication
+    fn mul(fn_id: i32, input: &[u8], output: &mut [u8]) -> Option<()> {
+        let res = match fn_id {
+            0x2a => {
+                <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::mul(input).ok()
+            }
+            0x2b => {
+                <megaclite::curves::Bls12_381 as megaclite::CurveBasicOperations>::mul(input).ok()
+            }
+            0x2c => <megaclite::curves::Bn254 as megaclite::CurveBasicOperations>::mul(input).ok(),
+            0x2d => {
+                <megaclite::curves::BW6_761 as megaclite::CurveBasicOperations>::mul(input).ok()
+            }
+            _ => None,
+        }?;
+
+        output.copy_from_slice(&res);
+        Some(())
     }
 
-    /// altbn_128 pairing
-    fn altbn_128_pairing(input: &[u8]) -> Option<bool> {
-        megaclite::altbn_128::pairing(input).ok()
-    }
+    /// Scalar Multiplication
+    fn pairing(fn_id: i32, input: &[u8]) -> Option<bool> {
+        let res = match fn_id {
+            0x2a => {
+                <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::pairing(input)
+                    .ok()
+            }
+            0x2b => {
+                <megaclite::curves::Bls12_381 as megaclite::CurveBasicOperations>::pairing(input)
+                    .ok()
+            }
+            0x2c => {
+                <megaclite::curves::Bn254 as megaclite::CurveBasicOperations>::pairing(input).ok()
+            }
+            0x2d => {
+                <megaclite::curves::BW6_761 as megaclite::CurveBasicOperations>::pairing(input).ok()
+            }
+            _ => None,
+        }?;
 
-    /// bls12_381 add
-    fn bls12_381_add(input: &[u8]) -> Option<[u8; 96]> {
-        megaclite::bls12_381::add(input).ok()
-    }
-
-    /// bls12_381 mul
-    fn bls12_381_mul(input: &[u8]) -> Option<[u8; 96]> {
-        megaclite::bls12_381::mul(input).ok()
-    }
-
-    /// bls12_381 pairing
-    fn bls12_381_pairing(input: &[u8]) -> Option<bool> {
-        megaclite::bls12_381::pairing(input).ok()
+        Some(res)
     }
 }
