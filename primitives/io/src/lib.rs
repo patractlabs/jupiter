5ef5a20b4016ef281e4583eb27787fa2b954a573
clearloop <udtrokia@gmail.com>
2020-12-08 11:39:14 +0800

    feat(primitives/io): add groth16 verification

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -3,12 +3,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use sp_runtime_interface::runtime_interface;
 
-/// ZK-SNARKs runtime interface
+pub mod groth16;
+
+/// Pairing runtime interface
 #[runtime_interface]
-pub trait ZkSnarks {
+pub trait Pairing {
     /// Vector Addition
-    fn add(fn_id: i32, input: &[u8]) -> Option<Vec<u8>> {
-        Some(match fn_id {
+    fn add(curve_id: i32, input: &[u8]) -> Option<Vec<u8>> {
+        Some(match curve_id {
             0x2a => {
                 <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::add(input).ok()
             }
@@ -24,8 +26,8 @@ pub trait ZkSnarks {
     }
 
     /// Scalar Multiplication
-    fn mul(fn_id: i32, input: &[u8]) -> Option<Vec<u8>> {
-        Some(match fn_id {
+    fn mul(curve_id: i32, input: &[u8]) -> Option<Vec<u8>> {
+        Some(match curve_id {
             0x2a => {
                 <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::mul(input).ok()
             }
@@ -40,22 +42,23 @@ pub trait ZkSnarks {
         }?)
     }
 
-    /// Scalar Multiplication
-    fn pairing(fn_id: i32, input: &[u8]) -> Option<bool> {
-        Some(match fn_id {
+    /// Pairing
+    fn pairing(curve_id: i32, input: &[u8]) -> Option<bool> {
+        Some(match curve_id {
             0x2a => {
-                <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::pairing(input)
+                <megaclite::curves::Bls12_377 as megaclite::CurveBasicOperations>::pairings(input)
                     .ok()
             }
             0x2b => {
-                <megaclite::curves::Bls12_381 as megaclite::CurveBasicOperations>::pairing(input)
+                <megaclite::curves::Bls12_381 as megaclite::CurveBasicOperations>::pairings(input)
                     .ok()
             }
             0x2c => {
-                <megaclite::curves::Bn254 as megaclite::CurveBasicOperations>::pairing(input).ok()
+                <megaclite::curves::Bn254 as megaclite::CurveBasicOperations>::pairings(input).ok()
             }
             0x2d => {
-                <megaclite::curves::BW6_761 as megaclite::CurveBasicOperations>::pairing(input).ok()
+                <megaclite::curves::BW6_761 as megaclite::CurveBasicOperations>::pairings(input)
+                    .ok()
             }
             _ => None,
         }?)
