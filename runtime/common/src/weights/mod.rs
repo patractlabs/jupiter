0ed256294880a6798f2da175ca5a294f6683fd03
Aten <jincxmain@gmail.com>
2020-12-17 14:56:38 +0800

    update substrate to 987a9723920217917f2708388d150add5ef52ef7 (#48)
    1. update substrate to 987a9723920217917f2708388d150add5ef52ef7 
    2. split Treasury module

--- runtime/common/src/weights/mod.rs
+++ runtime/common/src/weights/mod.rs
@@ -17,6 +17,7 @@
 /// A collection of weight modules used for pallets in the runtime.
 pub mod frame_system;
 pub mod pallet_balances;
+pub mod pallet_bounties;
 pub mod pallet_collective;
 pub mod pallet_democracy;
 pub mod pallet_elections_phragmen;
@@ -29,5 +30,6 @@ pub mod pallet_scheduler;
 pub mod pallet_session;
 pub mod pallet_staking;
 pub mod pallet_timestamp;
+pub mod pallet_tips;
 pub mod pallet_treasury;
 pub mod pallet_utility;
