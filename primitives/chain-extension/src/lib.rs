8901a2552d4c1719cd37a994a9b2864ef86a1b3c
clearloop <udtrokia@gmail.com>
2020-12-19 03:59:08 +0800

    feat(primitives/chain_extension): tests for curves

--- primitives/chain-extension/src/lib.rs
+++ primitives/chain-extension/src/lib.rs
@@ -1,10 +1,7 @@
 //! Jupiter Chain Extension
 #![cfg_attr(not(feature = "std"), no_std)]
-use pallet_contracts::{
-    chain_extension::{
-        ChainExtension, Environment, Ext, InitState, RetVal, SysConfig, UncheckedFrom,
-    },
-    Error,
+use pallet_contracts::chain_extension::{
+    ChainExtension, Environment, Ext, InitState, RetVal, SysConfig, UncheckedFrom,
 };
 use sp_runtime::DispatchError;
 use sp_std::vec::Vec;
@@ -13,31 +10,20 @@ use sp_std::vec::Vec;
 pub struct JupiterExt;
 
 impl ChainExtension for JupiterExt {
-    fn call<E: Ext>(func_id: u32, env: Environment<E, InitState>) -> Result<RetVal, DispatchError>
+    fn call<E: Ext>(
+        func_id: u32,
+        mut env: Environment<E, InitState>,
+    ) -> Result<RetVal, DispatchError>
     where
         <E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
     {
-        let mut dimmy: Vec<u8> = Vec::new();
-        let mut input = dimmy.as_mut_slice();
+        env.charge_weight(1_000_000_000_u64)?;
         let mut env = env.buf_in_buf_out();
-        env.read_into(&mut &mut input)?;
-        // if let Some(output) = if jupiter_io::pairing::wasm() {
-        //     curve::call(func_id, input)
-        // } else {
-        //     jupiter_io::pairing::call(func_id, input)
-        // } {
-        //     env.write(&output, false, None)?;
-        //     Ok(RetVal::Converging(0))
-        // } else {
-        //     Err(Error::<E::T>::InvalidFunctionId.into())
-        // }
-
-        if let Some(output) = curve::call(func_id, input) {
-            env.write(&output, false, None)?;
-            Ok(RetVal::Converging(0))
-        } else {
-            Err(Error::<E::T>::InvalidFunctionId.into())
-        }
+        let input: Vec<u8> = env.read_as()?;
+        let output = curve::call(func_id, &input)
+            .map_err(|_| DispatchError::Other("Call chain extension failed"))?;
+        env.write(&output, false, None)?;
+        Ok(RetVal::Converging(0))
     }
 
     fn enabled() -> bool {
