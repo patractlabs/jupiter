4e43a41befa5a32118c98a15d7925bf647785cec
zqh <zqhxuyuan@gmail.com>
2021-08-13 17:38:48 +0800

    reset spec version

--- runtime/common/src/impls.rs
+++ runtime/common/src/impls.rs
@@ -32,19 +32,16 @@ where
 
 pub struct ToRoot<R>(sp_std::marker::PhantomData<R>);
 impl<R> OnUnbalanced<NegativeImbalance<R>> for ToRoot<R>
-    where
-        R: pallet_balances::Config + pallet_sudo::Config,
-        <R as frame_system::Config>::AccountId: From<AccountId>,
-        <R as frame_system::Config>::AccountId: Into<AccountId>,
-        <R as frame_system::Config>::Event: From<pallet_balances::Event<R>>,
+where
+    R: pallet_balances::Config + pallet_sudo::Config,
+    <R as frame_system::Config>::AccountId: From<AccountId>,
+    <R as frame_system::Config>::AccountId: Into<AccountId>,
+    <R as frame_system::Config>::Event: From<pallet_balances::Event<R>>,
 {
     fn on_nonzero_unbalanced(amount: NegativeImbalance<R>) {
         let numeric_amount = amount.peek();
         let author = <pallet_sudo::Pallet<R>>::key();
-        <pallet_balances::Pallet<R>>::resolve_creating(
-            &author,
-            amount,
-        );
+        <pallet_balances::Pallet<R>>::resolve_creating(&author, amount);
         <frame_system::Pallet<R>>::deposit_event(pallet_balances::Event::Deposit(
             author,
             numeric_amount,
